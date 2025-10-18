b58550613234e1d100bbf9b10ba4d4bc3bd778a5
Umair Khan <umairk83@live.com>
2025-10-19 01:11:43 +0500

    Added the fetching of the coordinates

--- landing-page-fe/src/components/content-component/content-component.tsx
+++ landing-page-fe/src/components/content-component/content-component.tsx
@@ -1,7 +1,37 @@
+import  { useState, useEffect } from "react";
+
 function ContentComponent() {
+  const [location, setLocation] = useState<{ lat: number; lon: number } | null>(
+    null
+  );
+
+  // 👇 Automatically call on load
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      // Do something here
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation({ lat: latitude, lon: longitude });
+      },
+      (err) => {
+        // Dom something here
+      }
+    );
+  };
+
   return (
     <div>
       <h1> Content component </h1>
+      <div> Latitude {location?.lat} </div>
+      <div> Longitude {location?.lon} </div>
       <div data-role="counter" data-value="100">
         0
       </div>
