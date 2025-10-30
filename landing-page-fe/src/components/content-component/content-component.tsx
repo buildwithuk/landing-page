32771251359e80880a8ed1ffe045e6385e55e7e9
Uk <umairk83@live.com>
2025-10-30 12:53:56 +0500

    Moved the content to center

--- landing-page-fe/src/components/content-component/content-component.tsx
+++ landing-page-fe/src/components/content-component/content-component.tsx
@@ -1,12 +1,12 @@
 import type { FC, ReactElement } from "react";
 
-export const ContentComponent : FC = () : ReactElement => {
+export const ContentComponent: FC = (): ReactElement => {
   return (
-    <div>
-       <p className="font-semibold text-muted-foreground text-xl">
-      A modal dialog that interrupts the user with important content and expects
-      a response.
-    </p>
+    <div className="flex flex-row   items-center justify-center h-100 ">
+      <p className=" font-semibold text-muted-foreground text-xl">
+        A modal dialog that interrupts the user with important content and
+        expects a response.
+      </p>
     </div>
   );
-}
+};
