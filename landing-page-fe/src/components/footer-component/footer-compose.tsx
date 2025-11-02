8a724b59036ab7ea76d059de52b852d37ad31e0f
Uk <umairk83@live.com>
2025-11-02 21:51:17 +0500

    Added submit feedback

--- landing-page-fe/src/components/footer-component/footer-compose.tsx
+++ landing-page-fe/src/components/footer-component/footer-compose.tsx
@@ -1,16 +1,25 @@
-import { Separator } from "@radix-ui/react-separator";
-import { SquareCode } from "lucide-react";
+import { MessagesSquare, SquareCode } from "lucide-react";
 import type { ReactElement, FC } from "react";
+import { Button } from "../ui/button";
 
 export const FooterComponent: FC = (): ReactElement => {
   return (
-    <footer className="mt-2 flex flex-row justify-center ">
-      
+    <footer className="mt-2 flex flex-row content-center items-center">
       <div className="flex mr-3 content-center items-center">
         <SquareCode />
       </div>
       <div className="flex text-blue-600">
-        <p> <a href="https://github.com/buildwithuk/landing-page/tree/main/landing-page-fe"> https://github.com/buildwithuk </a> </p>
+        <p>
+          <a href="https://github.com/buildwithuk/landing-page/tree/main/landing-page-fe">
+            https://github.com/buildwithuk
+          </a>
+        </p>
+      </div>
+
+      <div>
+        <Button variant="outline" size="icon" className="rounded-full mx-5">
+          <MessagesSquare />
+        </Button>
       </div>
     </footer>
   );
